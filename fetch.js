8fbc45057a3c7f2440c5ea6b1600456d07a8dfbf
snehabandari123 <138192676+snehabandari123@users.noreply.github.com>
2023-08-01 10:16:14 +0530

    Delete fetch.js

--- fetch.js
+++
@@ -1,9 +0,0 @@
-async function fetchdata(){
-const temp = await fetch('https://api.spacexdata.com/v3/capsules');
-let result=await temp.json()
-console.log(result)
-return result
-}
-fetchdata()
-
-

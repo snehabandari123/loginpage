82aefa6ef5b8eabb7b13e65d68d3a72d5be4556a
snehabandari123 <138192676+snehabandari123@users.noreply.github.com>
2024-01-18 12:07:54 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,40 @@
-const loginForm = document.getElementById('loginForm');
-const message = document.getElementById('message');
-loginForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-const username = loginForm.username.value;
-const password = loginForm.password.value;
-  if (username === 'sneha' && password === '123') {
-    message.textContent = 'Login successful!';
-    window.location.href = 'home.html';
-
-  } else if(username === 'cloudplinth' && password === 'cloudplinth@123'){
-    message.textContent = 'Login successful!';
-  }
-  else {
-    message.textContent = 'Invalid credentials. Please try again.'; 
-  }
-loginForm.reset();
-});
+let loginFormEl=document.getElementById('loginForm');
+let usernameEl=document.getElementById('username');
+let passwordEl=document.getElementById('password');
+let messageEl=document.getElementById('message');
+let errorMsgNameEl=document.getElementById('errorMsgName');
+let errorMsgPasswordEl=document.getElementById('errorMsgPassword');
+usernameEl.addEventListener('blur',function(event){
+        if(event.target.value===''){
+            errorMsgNameEl.textContent='*required'
+        }
+        else{
+            errorMsgNameEl.textContent=''
+        }
+})
+passwordEl.addEventListener('blur',function(event){
+        if(event.target.value===''){
+            errorMsgPasswordEl.textContent='*required'
+        }
+        else{
+            errorMsgPasswordEl.textContent=''
+        }
+})
+function submitFunction(){
+        if (usernameEl.value==='sneha' && passwordEl.value==='sneha123'){
+            messageEl.textContent='login successful!!'
+            window.location.href = 'home.html';
+        }
+        else if (username.value === 'cloudplinth' && password.value === 'cloudplinth@123'){
+            messageEl.textContent='login successful!!'
+            window.location.href = 'home.html';
+        }
+        else{
+            messageEl.textContent = 'Invalid credentials. Please try again.'; 
+        }
+}
+loginFormEl.addEventListener('submit',function(event){
+    event.preventDefault();
+    submitFunction()
+    loginFormEl.reset()
+})
